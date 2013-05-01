fc84f8ed7f405674441c02222bf4607687bc0662
serztle <serztle@googlemail.com>
2013-05-01 21:27:53 +0200

    gui improved and querying over glyr implemented

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
+from threading import Thread
 from plyr import Query, PROVIDERS
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
+
+GObject.threads_init()
 
 
 class MetadataChooser:
-    def on_search_clicked(self, button):
-        self.search.set_visible(True)
+    def query_data(self):
+        self.data = self.query.commit()
+        print(self.query.error)
+        for c in self.data:
+            print(str(c.data, 'utf8'))
+        self.toggle_search()
+
+    def query_callback(self, cache, query):
         self.progress.pulse()
+        return 'ok'
+
+    def on_search_clicked(self, button):
+        self.toggle_search()
+        providers = []
+        for row in self.model:
+            if row[0]:
+                providers.append(row[1])
+
+        query = Query()
+        query.providers = providers
+        query.get_type = self.get_chosen_provider()
+        query.artist = self.builder.get_object("e_artist").get_text()
+        query.album = self.builder.get_object("e_album").get_text()
+        query.title = self.builder.get_object("e_title").get_text()
+        query.number = self.builder.get_object("adj_max").get_value()
+        query.verbosity = 0
+        self.query = query
+
+        Thread(target=self.query_data).start()
 
     def on_destroy(self, *args):
         Gtk.main_quit(*args)
@@ -19,34 +48,36 @@ class MetadataChooser:
             it = self.model.get_iter(path)
             self.model[it][0] = not self.model[it][0]
 
-    def on_col_clicked(self, col):
-        print("Hello")
+    def toggle_search(self):
+        self.search_panel.set_sensitive(not self.search_panel.get_sensitive())
+        self.combobox.set_sensitive(not self.combobox.get_sensitive())
+        self.provider.set_sensitive(not self.provider.get_sensitive())
+        self.search_progress.set_visible(not self.search_progress.get_visible())
 
-    def sort_column_quality(self, model, iter1, iter2, data):
-        if model[iter1][data] == model[iter2][data]:
-            return 0
-        elif model[iter1][data] > model[iter2][data]:
-            return 1
-        else:
-            return -1
-
-    def update_provider(self):
+    def get_chosen_provider(self):
         iter = self.combobox.get_active_iter()
         if iter is not None:
             model = self.combobox.get_model()
             key = model[iter][0]
-            details = PROVIDERS[key]
-            self.model = Gtk.ListStore(bool, str, str, str)
-            self.model.set_sort_func(0, self.sort_column_quality, 2)
-            self.model.set_sort_column_id(0, 1)
+            return key
+        return None
 
-            for p in details['providers']:
-                self.model.append([True, p['name'], str(p['quality']), str(p['speed'])])
-            self.provider.set_model(self.model)
-            self.update_entrys(details['required'])
+    def update_provider(self):
+        key = self.get_chosen_provider()
+        if key is not None:
+            providers = PROVIDERS[key]['providers']
+            required = PROVIDERS[key]['required']
+
+            model = Gtk.ListStore(bool, str, str, str, str)
+            for p in providers:
+                model.append([True, p['name'], str(p['quality']), str(p['speed']), ""])
+
+            self.model = model
+            self.provider.set_model(model)
+            self.update_entrys(required)
 
     def update_entry(self, required, key):
-        obj = self.builder.get_object("e_" + key)
+        obj = getattr(self, key)
         try:
             required.index(key)
             obj.set_sensitive(True)
@@ -61,22 +92,34 @@ class MetadataChooser:
     def __init__(self):
         self.builder = Gtk.Builder()
         self.builder.add_from_file("metadata-chooser.glade")
+        self.go = self.builder.get_object
 
-        self.window = self.builder.get_object("wd_metadata_chooser")
+        self.window = self.go("wd_metadata_chooser")
         self.window.connect("destroy", self.on_destroy)
 
-        self.search = self.builder.get_object("b_search")
+        self.view_results = self.go("tv_data")
+        self.artist = self.go("e_artist")
+        self.artist.set_text("DevilDriver")
 
-        self.search_button = self.builder.get_object("btn_search")
+        self.album = self.go("e_album")
+        self.album.set_text("Beast")
+
+        self.title = self.go("e_title")
+        self.title.set_text("Beast")
+
+        self.search_panel = self.go("g_search_data")
+        self.search_progress = self.go("b_search")
+
+        self.search_button = self.go("btn_search")
         self.search_button.connect("clicked", self.on_search_clicked)
 
-        self.adjustment = self.builder.get_object("adj_max")
-        self.adjustment.set_value(10)
+        self.adjustment = self.go("adj_max")
+        self.adjustment.set_value(1)
 
-        self.progress = self.builder.get_object("pb_search")
+        self.progress = self.go("pb_search")
         self.progress.set_pulse_step(0.05)
-        self.combobox = self.builder.get_object("cb_metadata_type")
 
+        self.combobox = self.go("cb_metadata_type")
         metadata_types = Gtk.ListStore(str)
         for p in sorted(PROVIDERS):
             metadata_types.append([p])
@@ -88,8 +131,7 @@ class MetadataChooser:
         self.combobox.set_active(0)
         self.combobox.connect("changed", self.on_type_changed)
 
-        self.provider = self.builder.get_object("tv_provider")
-
+        self.provider = self.go("tv_provider")
         cell = Gtk.CellRendererToggle()
         cell.connect("toggled", self.on_toggle, None)
         col = Gtk.TreeViewColumn("active", cell, active=0)
@@ -97,29 +139,27 @@ class MetadataChooser:
 
         cell = Gtk.CellRendererText()
         col = Gtk.TreeViewColumn("provider name", cell, text=1)
-        col.set_clickable(True)
-        col.connect("clicked", self.on_col_clicked)
+        col.set_sort_column_id(1)
         self.provider.append_column(col)
 
         cell = Gtk.CellRendererText()
         cell.set_fixed_size(50, -1)
         cell.set_alignment(1.0, 0.5)
         col = Gtk.TreeViewColumn("quality", cell, text=2)
-        col.set_clickable(True)
-        col.connect("clicked", self.on_col_clicked)
+        col.set_sort_column_id(2)
         self.provider.append_column(col)
 
         cell = Gtk.CellRendererText()
         cell.set_fixed_size(50, -1)
         cell.set_alignment(1.0, 0.5)
         col = Gtk.TreeViewColumn("speed", cell, text=3)
-        col.set_clickable(True)
-        col.connect("clicked", self.on_col_clicked)
+        col.set_sort_column_id(3)
+        self.provider.append_column(col)
+
+        col = Gtk.TreeViewColumn("", cell, text=4)
         self.provider.append_column(col)
 
         self.update_provider()
-
-        self.window = self.builder.get_object("wd_metadata_chooser")
         self.window.show()
 
 
